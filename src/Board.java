2766748a278620896537f0f7ed83aeb089fbb640
RaziZaman <145681484+RaziZaman@users.noreply.github.com>
2024-03-22 09:15:39 -0400

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Board {
     private String[][] board;
     public Board(int length) {
@@ -33,4 +35,51 @@ public class Board {
             System.out.println();
         }
     }
+    public void player1Choice() {
+        int placement = -1;
+        String alphabet[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        while (placement == -1) {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Player 1: ");
+            String choice = scan.nextLine();
+            for (int i = 0; i < board.length; i++) {
+                if (alphabet[i].equalsIgnoreCase(choice)) {
+                    placement = i;
+                }
+            }
+            if (placement == -1) {
+                System.out.println("Invalid Spot, Try Again");
+            }
+        }
+        for (int i = board[0].length - 1; i >= 0; i--) {
+            if (board[i][placement].equals(" ")) {
+                board[i][placement] = "X";
+                i = -1;
+            }
+        }
+    }
+    public void player2Choice() {
+        int placement = -1;
+        String alphabet[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        while (placement == -1) {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Player 2: ");
+            String choice = scan.nextLine();
+            for (int i = 0; i < board.length; i++) {
+                if (alphabet[i].equalsIgnoreCase(choice)) {
+                    placement = i;
+                }
+            }
+            if (placement == -1) {
+                System.out.println("Invalid Spot, Try Again");
+            }
+        }
+        for (int i = board[0].length - 1; i >= 0; i--) {
+            if (board[i][placement].equals(" ")) {
+                board[i][placement] = "O";
+                i = -1;
+            }
+        }
+    }
 }
+
